9bb997378732071b7b562bfd28915939ffaf3e0c
AP Leith, PhD <alex.leith@gmail.com>
2022-11-18 03:32:51 -0600

    New Layout

--- js/modal.js
+++ js/modal.js
@@ -1,42 +1,11 @@
-function dialog() {
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
 
-  // Declare variables
-  var dialogBox = $('.dialog'),
-      dialogTrigger = $('.dialog__trigger'),
-      dialogClose = $('.dialog__close'),
-      dialogTitle = $('.dialog__title'),
-      dialogContent = $('.dialog__content'),
-      dialogAction = $('.dialog__action');
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
 
-  // Open the dialog
-  dialogTrigger.on('click', function(e) {
-    dialogBox.toggleClass('dialog--active');
-    e.stopPropagation();
-  });
-
-  // Close the dialog - click close button
-  dialogClose.on('click', function() {
-    dialogBox.removeClass('dialog--active');
-  });
-
-  // Close the dialog - press escape key // key#27
-  $(document).keyup(function(e) {
-    if (e.keyCode === 27) {
-      dialogBox.removeClass('dialog--active');
-    }
-  });
-
-  // Close the dialog - click outside
-  $(document).on('click', function(e) {
-    if ($(e.target).is(dialogBox) === false &&
-        $(e.target).is(dialogTitle) === false &&
-        $(e.target).is(dialogContent) === false &&
-        $(e.target).is(dialogAction) === false) {
-      dialogBox.removeClass('dialog--active');   
-    }
-  });
-
-}
-
-// Run function
-$(dialog);// JavaScript Document
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
